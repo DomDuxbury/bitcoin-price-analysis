332688c05d20b9157316cc127c6889c5aae5ccef
DomDuxbury <domduxbury@hotmail.com>
2016-11-23 23:10:18 +0000

    Refactored correlation visualisation script and added non linear correlation

--- all_data_visualisation.py
+++ all_data_visualisation.py
@@ -18,9 +18,12 @@ def main():
             colors = ["red", "green", "black"])
 
     colors = [field.get_facecolor()[0] for field in fields]
+
     patch1=mpl.patches.Patch(color=colors[0], label= 'Negative')
     patch2=mpl.patches.Patch(color=colors[1], label ='Positive')
     patch3=mpl.patches.Patch(color=colors[2], label ='Spam')
+    
+    plt.ylabel("Total Tweets")
     plt.legend(handles=[patch1,patch2, patch3])
     plt.show()
 
--- sentiment_correlation.py
+++ sentiment_correlation.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import utils
+import scipy.spatial as sc
 
 def main():
     price = utils.getPriceData()
     labelledTweets = utils.getAllTweetsAggregated() 
+    labelledTweets["total"] = labelledTweets["pos"] + labelledTweets["neg"] + labelledTweets["spam"]
 
     df = labelledTweets.join(price).dropna()
     
@@ -14,27 +16,33 @@ def main():
         df['hour ' + str(hour)] = df['Close Price'].shift(-1 * hour) - df['Close Price']
 
     df = df.dropna()
-
+    
+    df["ratio"] = utils.compareToDailyCycle(df)
     df["pos_ratio"] = utils.compareToDailyCycle(df, "pos")
     df["neg_ratio"] = utils.compareToDailyCycle(df, "neg")
     df["spam_ratio"] = utils.compareToDailyCycle(df, "spam")
   
-    negcorrelation = []
-    poscorrelation = []
-    spamcorrelation = []
-    peakcorrelation = []
+    linear_corr = lambda x,y : np.corrcoef(x,y)[0,1]
+    correlation(df, linear_corr).plot()
+    correlation(df, utils.distcorr).plot()
 
-    for hour in range(1, 25):
-        negcorrelation.append(np.corrcoef(df['hour ' + str(hour)], df['neg_ratio'])[0,1])
-        poscorrelation.append(np.corrcoef(df['hour ' + str(hour)], df['pos_ratio'])[0,1])
-        spamcorrelation.append(np.corrcoef(df['hour ' + str(hour)], df['spam_ratio'])[0,1])
-    
-    pd.DataFrame(dict(
-        pos = poscorrelation, 
-        neg = negcorrelation, 
-        spam = spamcorrelation)).plot()
-
+    plt.ylabel("Correlation Coefficient")
     plt.show()
     
+def correlation(df, func):
+    neg, pos, spam, total = [], [], [], []
+    for hour in range(1, 25):
+        hourString = "hour " + str(hour)  
+        neg.append(func(df[hourString], df['neg_ratio']))
+        pos.append(func(df[hourString], df['pos_ratio']))
+        spam.append(func(df[hourString], df['spam_ratio']))
+        total.append(func(df[hourString], df['ratio']))
+            
+    return pd.DataFrame(dict(
+        pos = pos,
+        neg = neg,
+        spam = spam,
+        total = total))
+
 if __name__ == "__main__":
     main()
